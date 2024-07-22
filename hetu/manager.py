035e61246ba9739da9f78bab3bb52b9ead500edd
Heerozh <heeroz@gmail.com>
2024-07-22 21:34:50 +0800

    ENH: 结构优化，去掉单件

--- hetu/manager.py
+++ hetu/manager.py
@@ -4,32 +4,29 @@
 @license: Apache2.0 可用作商业项目，再随便找个角落提及用到了此项目 :D
 @email: heeroz@gmail.com
 """
-from hetu.data.component import BaseComponent
-from hetu.data.backend.base import Backend, ComponentTable
-from hetu.common import Singleton
-from hetu.system import SystemClusters
 from typing import ItemsView
 
+from hetu.data.backend.base import Backend, ComponentTable
+from hetu.data.component import BaseComponent
+from hetu.system import SystemClusters
 
-class ComponentTableManager(metaclass=Singleton):
+
+class ComponentTableManager:
     """
     ComponentTable管理类，负责对每个ComponentTable的初始化和获取。
-    此类是单件，每个进程只有一个实例。
+    此类只能在SystemCluster.build_clusters()后初始化
     """
-    def __init__(self):
-        self.tables = {}
-        self.tables_by_name = {}
-        self.subscriptions = {}
 
-    def build(
+    def __init__(
             self,
             namespace: str,
             instance_name: str,
             backends: dict[str, Backend],
-            table_constructors: dict[str, type[ComponentTable]],
-            check_schema: bool = True
+            table_constructors: dict[str, type[ComponentTable]]
     ):
-        """初始化所有ComponentTable的实例，此方法只能在SystemCluster.build_clusters()后调用。"""
+        self.tables = {}
+        self.tables_by_name = {}
+
         clusters = SystemClusters().get_clusters(namespace)
         for cluster in clusters:
             for comp in cluster.components:
@@ -37,9 +34,18 @@ class ComponentTableManager(metaclass=Singleton):
                 table_constructor = table_constructors.get(comp.backend_)
                 if backend is None or table_constructor is None:
                     raise ValueError(f"Backend {comp.backend_} not found")
-                table = table_constructor(comp, instance_name, cluster.id, backend, check_schema)
+                table = table_constructor(comp, instance_name, cluster.id, backend)
                 self.tables[comp] = table
                 self.tables_by_name[comp.component_name_] = table
+
+    def create_or_migrate_all(self):
+        for tbl in self.tables.values():
+            tbl.create_or_migrate()
+
+    def flush_volatile(self):
+        for comp, tbl in self.tables.items():
+            if not comp.persist_:
+                tbl.flush()
 
     def get_table(self, component_cls: type[BaseComponent] | str) -> ComponentTable | None:
         if type(component_cls) is str:
