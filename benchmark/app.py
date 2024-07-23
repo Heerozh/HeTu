db1de9b7f6e744e993a4c1deb65366f49d2a191b
Heerozh <heeroz@gmail.com>
2024-07-23 20:27:22 +0800

    ENH: bench完成

--- benchmark/app.py
+++ benchmark/app.py
@@ -5,34 +5,36 @@ import string
 
 
 @hetu.data.define_component(namespace='bench')
-class NameTable(hetu.data.BaseComponent):
+class StrTable(hetu.data.BaseComponent):
     name: '<U16' = hetu.data.Property('', unique=True)
     number: np.int32 = hetu.data.Property(0)
 
 
 @hetu.data.define_component(namespace='bench')
-class NumberTable(hetu.data.BaseComponent):
+class IntTable(hetu.data.BaseComponent):
     number: np.int32 = hetu.data.Property(0, unique=True)
     name: '<U16' = hetu.data.Property('Unnamed')
 
 
-@hetu.system.define_system(namespace='bench', components=(NumberTable, ),
+@hetu.system.define_system(namespace='bench', components=(IntTable, ),
                            permission=hetu.data.Permission.EVERYBODY)
 async def select_and_update(ctx: hetu.system.Context, number):
-    async with ctx[NumberTable].select_or_create(number, 'number') as row:
+    async with ctx[IntTable].select_or_create(number, 'number') as row:
         row.name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
     return hetu.system.ResponseToClient([number])
 
 
-@hetu.system.define_system(namespace='bench', components=(NameTable, NumberTable),
+@hetu.system.define_system(namespace='bench', components=(StrTable, IntTable),
                            permission=hetu.data.Permission.EVERYBODY)
 async def exchange_data(ctx: hetu.system.Context, name, number):
-    async with ctx[NameTable].select_or_create(name, 'name') as name_row:
-        async with ctx[NumberTable].select_or_create(number, 'number') as number_row:
+    async with ctx[StrTable].select_or_create(name, 'name') as name_row:
+        async with ctx[IntTable].select_or_create(number, 'number') as number_row:
             name_row.number, number_row.name = number, name
+    return hetu.system.ResponseToClient([number])
 
 
 @hetu.system.define_system(namespace="bench", permission=hetu.data.Permission.EVERYBODY,
                            inherits=('elevate',))
 async def login_test(ctx: hetu.system.Context, user_id):
     await ctx['elevate'](ctx, user_id, kick_logged_in=True)
+    return hetu.system.ResponseToClient([user_id])
