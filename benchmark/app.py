b22bb73533e03e44c643234db551753c3395092d
Heerozh <heeroz@gmail.com>
2024-07-24 19:36:01 +0800

    TST: bench显示下事务冲突率

--- benchmark/app.py
+++ benchmark/app.py
@@ -4,13 +4,13 @@ import random
 import string
 
 
-@hetu.data.define_component(namespace='bench')
+@hetu.data.define_component(namespace='bench', persist=False)
 class StrTable(hetu.data.BaseComponent):
     name: '<U16' = hetu.data.Property('', unique=True)
     number: np.int32 = hetu.data.Property(0)
 
 
-@hetu.data.define_component(namespace='bench')
+@hetu.data.define_component(namespace='bench', persist=False)
 class IntTable(hetu.data.BaseComponent):
     number: np.int32 = hetu.data.Property(0, unique=True)
     name: '<U16' = hetu.data.Property('Unnamed')
@@ -21,7 +21,7 @@ class IntTable(hetu.data.BaseComponent):
 async def select_and_update(ctx: hetu.system.Context, number):
     async with ctx[IntTable].select_or_create(number, 'number') as row:
         row.name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
-    return hetu.system.ResponseToClient([number])
+    return hetu.system.ResponseToClient([ctx.retry_count])
 
 
 @hetu.system.define_system(namespace='bench', components=(StrTable, IntTable),
@@ -30,11 +30,11 @@ async def exchange_data(ctx: hetu.system.Context, name, number):
     async with ctx[StrTable].select_or_create(name, 'name') as name_row:
         async with ctx[IntTable].select_or_create(number, 'number') as number_row:
             name_row.number, number_row.name = number, name
-    return hetu.system.ResponseToClient([number])
+    return hetu.system.ResponseToClient([ctx.retry_count])
 
 
 @hetu.system.define_system(namespace="bench", permission=hetu.data.Permission.EVERYBODY,
                            inherits=('elevate',))
 async def login_test(ctx: hetu.system.Context, user_id):
     await ctx['elevate'](ctx, user_id, kick_logged_in=True)
-    return hetu.system.ResponseToClient([user_id])
+    return hetu.system.ResponseToClient([ctx.retry_count])
