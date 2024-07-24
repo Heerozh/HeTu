8d702c09f7f666d33b8bfffb7fbd1203c50a7bdd
Heerozh <heeroz@gmail.com>
2024-07-24 23:58:45 +0800

    TST: pubsub bench初步

--- benchmark/test_bench.py
+++ benchmark/test_bench.py
@@ -17,7 +17,7 @@ import websockets
 BENCH_ROW_COUNT = 30000
 
 
-async def bench_sys_call_routine(address, duration, pid, name, packet):
+async def bench_sys_call_routine(address, duration, name: str, pid: str, packet):
     call_count = defaultdict(int)
     retry_count = defaultdict(int)
     if address.startswith('wss://'):
@@ -55,37 +55,60 @@ async def bench_sys_call_routine(address, duration, pid, name, packet):
     return call_count, retry_count
 
 
-async def bench_login(address, duration, name, pid):
-    def packet():
-        user_id = random.randint(1, BENCH_ROW_COUNT)
-        return ['sys', 'login_test', user_id]
+async def bench_pubsub_routine(address, duration, name: str, pid: str):
+    recv_count = defaultdict(int)
+    if address.startswith('wss://'):
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = False
+        ssl_context.verify_mode = ssl.CERT_NONE  # 应该使用ssl_context.load_cert_chain(cert_file)更安全
+    else:
+        ssl_context = None
+    print(name, '开始测试', pid, '号客户端', duration, '分钟运行时间')
+    try:
+        async with websockets.connect(address, ssl=ssl_context) as ws:
+            # 组合封包
+            raw_call = ['sub', 'IntTable', 'query', 'number', 0, 100, 100]
+            call = json.dumps(raw_call).encode()
+            call = zlib.compress(call)
+            await ws.send(call)
+            while True:
+                # 统计收到的消息数
+                _ = await ws.recv()
+                # 记录当前分钟的执行数
+                cur_min = int(time.time() // 60)
+                recv_count[cur_min] += 1
+                if len(recv_count) > duration:
+                    del recv_count[cur_min]
+                    break
+    except websockets.exceptions.ConnectionClosedError:
+        print(pid, '号客户端连接断开，提前结束测试')
+        pass
+    return recv_count
 
-    return await bench_sys_call_routine(address, duration, pid, name, packet)
 
-
-async def bench_select_update(address, duration, name, pid):
+async def bench_select_update(address, duration, name: str, pid: str):
     def packet():
         row_id = random.randint(1, BENCH_ROW_COUNT)
         return ['sys', 'select_and_update', row_id]
 
-    return await bench_sys_call_routine(address, duration, pid, name, packet)
+    return await bench_sys_call_routine(address, duration, name, pid, packet)
 
 
-async def bench_exchange_data(address, duration, name, pid):
+async def bench_exchange_data(address, duration, name: str, pid: str):
     def packet():
         rnd_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
         row_id = random.randint(1, BENCH_ROW_COUNT)
         return ['sys', 'exchange_data', rnd_str, row_id]
 
-    return await bench_sys_call_routine(address, duration, pid, name, packet)
+    return await bench_sys_call_routine(address, duration, name, pid, packet)
 
 
-async def gather_clients(func, client_num, pid):
-    clients = [func(pid) for i in range(client_num)]
+async def gather_clients(func, client_num, pid: int):
+    clients = [func(f"{pid}.{i}") for i in range(client_num)]
     return await asyncio.gather(*clients)
 
 
-def run_client(func, client_num, pid):
+def run_client(func, client_num, pid: int):
     return asyncio.run(gather_clients(func, client_num, pid))
 
 
@@ -120,7 +143,8 @@ def stat_count(list3d, test_name):
 def run_bench(func, _args, name):
     cpu = multiprocessing.cpu_count()
     process_num = cpu * 2
-    clients = int(_args.clients // cpu)
+    process_num = _args.clients if process_num > _args.clients else process_num
+    clients = min(1, int(_args.clients // process_num))
 
     atest = partial(func, _args.address, _args.time, name)
     test = partial(run_client, atest, clients)
@@ -136,27 +160,38 @@ if __name__ == '__main__':
     # 此方法需要手动部署hetu服务器和backend，和测试机分开
     parser = argparse.ArgumentParser(prog='hetu', description='Hetu full bench')
     parser.add_argument("--address", type=str, default="ws://127.0.0.1:2466/hetu/")
+    parser.add_argument("--item", default='call', help="或者填pubsub测试推送能力")
     parser.add_argument("--clients", type=int, default=200, help="启动客户端数")
     parser.add_argument("--time", type=int, default=5,
                         help="每项目测试时间（分钟），测试结果去头尾各1分钟，所以要3分钟起")
     args = parser.parse_args()
 
     # 分配clients个进程和连接进行测试
-    all_results = [
-        run_bench(bench_login, args, 'login'),
-        run_bench(bench_select_update, args, 'select + update'),
-        run_bench(bench_exchange_data, args, 'select*2 + update*2')
-    ]
+    match args.item:
+        case 'call':
+            all_results = [
+                run_bench(bench_select_update, args, 'select + update'),
+                run_bench(bench_exchange_data, args, 'select*2 + update*2')
+            ]
+        case 'pubsub':
+            all_results = [
+                run_bench(bench_pubsub_routine, args, 'pubsub')
+            ]
+        case _:
+            raise ValueError('--item未知测试项目')
 
     # 汇总统计
     stat_df = pd.concat(all_results, axis=1)
     stat_df.loc['Avg'] = stat_df.mean()
 
     cpm_stat = stat_df.loc[:, stat_df.columns.str.contains('CPM')].copy()
-    cpm_stat.loc['RTT(ms)'] = 60 / cpm_stat.loc['Avg'] * 1000 * args.clients
+    if args.item == 'call':
+        cpm_stat.loc['RTT(ms)'] = 60 / cpm_stat.loc['Avg'] * 1000 * args.clients
 
     race_stat = stat_df.loc[:, stat_df.columns.str.contains('Race')]
     print("各项目每分钟执行次数：")
     print(cpm_stat.to_markdown())
-    print("各项目事务冲突率：")
-    print(race_stat.to_markdown())
+
+    if args.item == 'call':
+        print("各项目事务冲突率：")
+        print(race_stat.to_markdown())
