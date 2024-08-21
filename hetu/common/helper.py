c39c85ad5d0d2d5bd48b9faca11e2f069177076a
Heerozh <heeroz@gmail.com>
2024-08-21 18:34:04 +0800

    MAINT: typo, move code

--- hetu/common/helper.py
+++ hetu/common/helper.py
@@ -10,3 +10,24 @@ def batched(iterable, n):
     while batch := tuple(itertools.islice(it, n)):
         yield batch
 
+
+def resolve_import(s):
+    """
+    Resolve strings to objects using standard import and attribute
+    syntax.
+    """
+    name = s.split('.')
+    used = name.pop(0)
+    try:
+        found = __import__(used)
+        for frag in name:
+            used += '.' + frag
+            try:
+                found = getattr(found, frag)
+            except AttributeError:
+                __import__(used)
+                found = getattr(found, frag)
+        return found
+    except ImportError as e:
+        v = ValueError('Cannot resolve %r: %s' % (s, e))
+        raise v from e
