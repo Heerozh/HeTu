fdf51c0d08752fb4925d92cd744df33061c0cbcc
Heerozh <heeroz@gmail.com>
2024-08-23 15:54:56 +0800

    MAINT: typo

--- hetu/system/context.py
+++ hetu/system/context.py
@@ -1,7 +1,7 @@
 """
 @author: Heerozh (Zhang Jianhao)
 @copyright: Copyright 2024, Heerozh. All rights reserved.
-@license: MIT 可用作商业项目，再随便找个角落提及用到了此项目 :D
+@license: Apache2.0 可用作商业项目，再随便找个角落提及用到了此项目 :D
 @email: heeroz@gmail.com
 """
 from dataclasses import dataclass
