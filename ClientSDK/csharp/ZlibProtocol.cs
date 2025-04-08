0c4cfe04014ea73a700563b0c4a0eab0381eaa4e
Heerozh <heeroz@gmail.com>
2025-04-08 23:32:53 +0800

    MAINT: c# sdk单独移出来

---
+++ ClientSDK/csharp/ZlibProtocol.cs
@@ -0,0 +1,32 @@
+﻿using Unity.SharpZipLib.Zip.Compression.Streams;
+using System.IO;
+
+namespace HeTu
+{
+    /// <summary>
+    /// 默认的消息压缩协议
+    /// </summary>
+    public class ZlibProtocol: IProtocol
+    {
+        public byte[] Compress(byte[] data)
+        {
+            var zipBuff = new MemoryStream();
+            var zipStream = new DeflaterOutputStream(zipBuff);
+            var input = new MemoryStream(data);
+            input.CopyTo(zipStream);
+            zipStream.Finish();
+            return zipBuff.ToArray();
+        }
+
+        public byte[] Decompress(byte[] data)
+        {
+            var unzipBuff = new MemoryStream();
+            var input = new MemoryStream(data);
+            var unzipStream = new InflaterInputStream(input);
+            unzipStream.CopyTo(unzipBuff);
+            return unzipBuff.ToArray();
+        }
+
+    }
+}
+
