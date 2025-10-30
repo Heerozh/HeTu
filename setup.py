4094f8b67f5e465ebc8c81bb943087a13b93d295
heerozh <heeroz@gmail.com>
2025-10-30 14:42:54 +0800

    CI: upgrade py3.13 & 精简image

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.12.5',
+    python_requires='>=3.13',
     install_requires=requirements,
 
     version=re.findall(r"^__version__ = \"([^']+)\"\r?$",
