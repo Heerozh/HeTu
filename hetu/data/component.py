56d96ad33a00e57127f19385dd2c31d1afd5cebe
heerozh <heeroz@gmail.com>
2025-11-07 21:30:00 +0800

    ENH: rls定义完成

--- hetu/data/component.py
+++ hetu/data/component.py
@@ -27,8 +27,8 @@ logger = logging.getLogger('HeTu.root')
 class Permission(IntEnum):
     EVERYBODY = 1
     USER = 2
-    OWNER = 3  # 同RLS权限，只是预设了RLS_COMPARE为(equal, 'owner', 'caller')
-    RLS = 4  # 由RLS_COMPARE参数(func, ComponentPropertyName, ContextPropertyName)决定具体的rls逻辑
+    OWNER = 3  # 同RLS权限，只是预设了rls_compare为(equal, 'owner', 'caller')
+    RLS = 4  # 由rls_compare参数(func, component_property_name, context_property_name)决定具体的rls逻辑
     ADMIN = 999
 
 
@@ -46,7 +46,7 @@ class BaseComponent:
     component_name_ = None
     namespace_ = None
     permission_ = Permission.USER
-    rls_compare_ = (operator.eq, 'owner', 'caller')     # type: tuple[Callable[[Any, Any], bool], str, str]
+    rls_compare_ = None     # type: tuple[Callable[[Any, Any], bool], str, str]
     persist_ = True                                     # 只是标记，每次启动时会清空此标记的数据
     readonly_ = False                                   # 只是标记，调用写入会警告
     backend_ = None         # type: str                 # 该Component由哪个后端(数据库)负责储存和查询
@@ -192,6 +192,7 @@ def define_component(
     persist=True,
     readonly=False,
     backend: str = "default",
+    rls_compare: tuple[Callable[[Any, Any], bool], str, str] = None,
 ):
     """
     定义Component组件（表）的数据结构
@@ -222,7 +223,12 @@ def define_component(
         - everybody: 任何客户端连接都可以读，适合读一些服务器状态类的数据，如在线人数
         - user: 只有已登录的客户端都连接可以读
         - admin: 只有管理员权限客户端连接可以读
-        - owner: 只能读取到owner属性值==登录的用户id（`ctx.caller`）的行，未登录的客户端无法读取
+        - owner: 只能读取到owner属性值==登录的用户id（`ctx.caller`）的行，未登录的客户端无法读取。
+                 此权限等同rls权限，且`rls_compare=(operator.eq, 'owner', 'caller')`
+        - rls: 行级权限，需要配合rls_compare参数使用，定义具体的行级权限逻辑
+    rls_compare:
+        当permission设置为RLS(行级权限)时，定义行级安全的比较函数和属性名，格式为(func, 表属性名, context属性名)，
+        只有func(row.表属性名, ctx.context属性名)返回True时允许读取此行。如果属性不存在，按nan处理（无法和任何值比较）。
     force: bool
         强制覆盖同名Component，单元测试用。
     _cls: class
@@ -305,11 +311,23 @@ def define_component(
         assert issubclass(cls, BaseComponent), f"{cls.__name__}必须继承于BaseComponent"
 
         # 检查OWNER权限必须有owner属性
+        nonlocal rls_compare
         if permission == Permission.OWNER:
+            assert rls_compare is None, \
+                f"{cls.__name__}权限设置为OWNER时，不能再设置rls_compare参数"
+            # 修改闭包外的变量rls_compare
+            rls_compare = (operator.eq, 'owner', 'caller')
             assert 'owner' in properties, \
                 f"{cls.__name__}权限设置为OWNER时，必须有owner属性，该属性表明此条数据属于哪个用户"
             assert np.issubdtype(properties['owner'].dtype, np.number), \
                 f"{cls.__name__}的owner属性必需是numeric数字(int, np.int64, ...)类型"
+
+        # 检查RLS定义
+        if permission == Permission.RLS:
+            assert rls_compare is not None, \
+                f"{cls.__name__}权限设置为RLS时，必须设置rls_compare参数，定义行级权限逻辑"
+            assert rls_compare[1] in properties, \
+                f"{cls.__name__}权限设置为RLS: {rls_compare}，但表没有定义{rls_compare[1]}属性"
 
         # 生成json格式，并通过json加载到class中
         json_str = BaseComponent.make_json(properties, namespace, cls.__name__, permission,
