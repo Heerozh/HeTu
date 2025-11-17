ff1153d6851b8ca385e33c0f8508fc461ceb4733
heerozh <heeroz@gmail.com>
2025-11-17 23:56:03 +0800

    TST: 迁移test_auto_migration

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -1,61 +1,84 @@
+import numpy as np
+import pytest
 
 
+async def test_migration_unique_violation(filled_item_table):
+    # 测试自动迁移
+    backend = filled_item_table.backend
+    table_cls = filled_item_table.__class__
 
-#
-#
-#     @parameterized(implements)
-#     async def test_migration(self, table_cls: type[ComponentTable],
-#                              backend_cls: type[Backend], config):
-#         # 测试迁移，先用原定义写入数据
-#         backend = backend_cls(config)
-#         item_data = table_cls(Item, 'test', 1, backend)
-#         item_data.create_or_migrate()
-#         item_data.flush(force=True)
-#
-#         async with backend.transaction(1) as session:
-#             tbl = item_data.attach(session)
-#             for i in range(25):
-#                 row = Item.new_row()
-#                 row.id = 0
-#                 row.name = f'Itm{i + 10}aaaaaaaaaa'
-#                 row.owner = 10
-#                 row.time = i + 110
-#                 row.qty = 999
-#                 await tbl.insert(row)
-#
-#         # 重新定义新的属性
-#         ComponentDefines().clear_()
-#
-#         @define_component(namespace="ssw")
-#         class ItemNew(BaseComponent):
-#             owner: np.int64 = Property(0, unique=False, index=True)
-#             model: np.int32 = Property(0, unique=False, index=True)
-#             qty_new: np.int16 = Property(111, unique=False, index=False)
-#             level: np.int8 = Property(1, unique=False, index=False)
-#             time: np.int64 = Property(0, unique=True, index=True)
-#             name: 'U6' = Property("", unique=True, index=False)
-#             used: bool = Property(False, unique=False, index=True)
-#
-#         # 从ItemNew改名回Item，以便迁移同名的
-#         import json
-#         define = json.loads(ItemNew.json_)
-#         define['component_name'] = 'Item'
-#         renamed_new_item_cls = BaseComponent.load_json(json.dumps(define))
-#
-#         # 测试迁移
-#         item_data = table_cls(renamed_new_item_cls, 'test', 2, backend)
-#         item_data.create_or_migrate()
-#         # 检测跨cluster报错
-#         with self.assertRaisesRegex(AssertionError, "cluster"):
-#             async with backend.transaction(1) as session:
-#                 item_data.attach(session)
-#
-#         async with backend.transaction(2) as session:
-#             tbl = item_data.attach(session)
-#             assert (await tbl.select(111, where='time')).name == 'Itm11a'
-#             assert (await tbl.select(111, where='time')).qty_new == 111
-#
-#             assert (await tbl.select('Itm30a', where='name')).name == 'Itm30a'
-#             assert (await tbl.select(130, where='time')).qty_new == 111
-#
-#         await backend.close()
+    # 重新定义新的属性
+    from hetu.data import define_component, Property, BaseComponent, ComponentDefines
+    ComponentDefines().clear_()
+
+    @define_component(namespace="pytest")
+    class ItemNew(BaseComponent):
+        owner: np.int64 = Property(0, unique=False, index=True)
+        model: np.int32 = Property(0, unique=False, index=True)
+        qty_new: np.int16 = Property(111, unique=False, index=False)
+        level: np.int8 = Property(1, unique=False, index=False)
+        time: np.int64 = Property(0, unique=True, index=True)
+        name: 'U4' = Property("", unique=True, index=False)
+        used: bool = Property(False, unique=False, index=True)
+
+    # 从ItemNew改名回Item，以便迁移同名的
+    import json
+    define = json.loads(ItemNew.json_)
+    define['component_name'] = 'Item'
+    renamed_new_item_cls = BaseComponent.load_json(json.dumps(define))
+
+    # 测试迁移
+    new_item_table = table_cls(renamed_new_item_cls, 'ItemTestTable', 2, backend)
+
+    # item.name是U4截断，导致unique违反，不能迁移
+    with pytest.raises(RuntimeError, match="unique"):
+        new_item_table.create_or_migrate()
+
+
+async def test_auto_migration(filled_item_table):
+    # 测试自动迁移
+    backend = filled_item_table.backend
+    table_cls = filled_item_table.__class__
+
+    # 重新定义新的属性
+    from hetu.data import define_component, Property, BaseComponent, ComponentDefines
+    ComponentDefines().clear_()
+
+    @define_component(namespace="pytest")
+    class ItemNew(BaseComponent):
+        owner: np.int64 = Property(0, unique=False, index=True)
+        model: np.int32 = Property(0, unique=False, index=True)
+        qty_new: np.int16 = Property(111, unique=False, index=False)
+        level: np.int8 = Property(1, unique=False, index=False)
+        time: np.int64 = Property(0, unique=True, index=True)
+        name: 'U4' = Property("", unique=False, index=True)
+        used: bool = Property(False, unique=False, index=True)
+
+    # 从ItemNew改名回Item，以便迁移同名的
+    import json
+    define = json.loads(ItemNew.json_)
+    define['component_name'] = 'Item'
+    renamed_new_item_cls = BaseComponent.load_json(json.dumps(define))
+
+    # 测试迁移
+    new_item_table = table_cls(renamed_new_item_cls, 'ItemTestTable', 2, backend)
+
+    new_item_table.create_or_migrate()
+
+    # 检测跨cluster报错
+    with pytest.raises(AssertionError, match="cluster"):
+        async with backend.transaction(1) as session:
+            new_item_table.attach(session)
+
+    async with (backend.transaction(2) as session):
+        tbl = new_item_table.attach(session)
+        assert (await tbl.select(111, where='time')).name == 'Itm1'
+        assert (await tbl.select(111, where='time')).qty_new == 111
+        assert (await tbl.select(111, where='time')).qty_new == 111
+
+        assert (await tbl.select('Itm3', where='name')).name == 'Itm3'
+        # 截断后有重复值了
+        np.testing.assert_array_equal(
+            (await tbl.query("name", 'Itm3')).time,
+            [130, 131, 132, 133, 134]
+        )
