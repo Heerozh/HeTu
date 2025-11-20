efccd87cb37100c2ae0b9282b4f32cdb2095e9c6
heerozh <heeroz@gmail.com>
2025-11-20 21:43:10 +0800

    TST: 只剩websockt和connect测试了

---
+++ tests/test_system_connection.py
@@ -0,0 +1,6 @@
+# todo 测试连接，包括flood检测等，特别是future不应该遇到flood检测
+
+# async def test_connect_with_system_executor(comp_mgr):
+#     from hetu.system import SystemExecutor
+#     executor = SystemExecutor('ssw', comp_mgr)
+#     await executor.initialize("")
