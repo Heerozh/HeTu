d6cafc2bdc13c6a53ea52628251ed5ed04c8800c
heerozh <heeroz@gmail.com>
2025-11-20 18:56:15 +0800

    MAINT: lock方法要放在一起

--- hetu/system/future.py
+++ hetu/system/future.py
@@ -6,7 +6,6 @@
 """
 
 import asyncio
-import datetime
 import logging
 import random
 import time
@@ -17,7 +16,7 @@ import warnings
 
 from .context import Context
 from .definer import define_system, SystemClusters, SYSTEM_NAME_MAX_LEN
-from .execution import ExecutionLock
+from .execution import ExecutionLock, clean_expired_call_locks
 from .executor import SystemExecutor
 from ..data import BaseComponent, define_component, Property, Permission
 from ..data.backend import ComponentTable
@@ -160,35 +159,6 @@ async def create_future_call(
         row.timeout = timeout
 
     return _uuid
-
-
-async def clean_expired_call_locks(comp_mgr):
-    """清空超过7天的call_lock的已执行uuid数据，只有服务器非正常关闭才可能遗留这些数据，因此只需服务器启动时调用。"""
-    duplicates = ExecutionLock.get_duplicates(comp_mgr.namespace).values()
-    for comp in [ExecutionLock] + list(duplicates):
-        tbl = comp_mgr.get_table(comp)
-        if tbl is None:  # 说明项目没任何地方引用此Component
-            continue
-        backend = tbl.backend
-        deleted = 0
-        while True:
-            async with backend.transaction(tbl.cluster_id) as session:
-                tbl_trx = tbl.attach(session)
-                rows = await tbl_trx.query(
-                    "called",
-                    left=0,
-                    right=time.time() - datetime.timedelta(days=7).total_seconds(),
-                    limit=1000,
-                )
-                # 循环每行数据，删除
-                for row in rows:
-                    await tbl_trx.delete(row.id)
-                deleted += len(rows)
-                if len(rows) == 0:
-                    break
-        logger.info(
-            f"🔗 [⚙️Future] 释放了 {comp.component_name_} 的 {deleted} 条过期数据"
-        )
 
 
 async def sleep_for_upcoming(tbl: ComponentTable):
