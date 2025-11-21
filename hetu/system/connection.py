ce13a59de344df30723ee56c77322ca61354c76b
heerozh <heeroz@gmail.com>
2025-11-21 17:17:50 +0800

    TST: 测试连接flood

--- hetu/system/connection.py
+++ hetu/system/connection.py
@@ -39,8 +39,8 @@ class Connection(BaseComponent):
     namespace="global", permission=Permission.ADMIN, components=(Connection,)
 )
 async def new_connection(ctx: Context, address: str):
-    # todo bug: 服务器自己的（future call之类的localhost）连接不应该受IP限制
-    if MAX_ANONYMOUS_CONNECTION_BY_IP:
+    # 服务器自己的（future call之类的localhost）连接不应该受IP限制
+    if MAX_ANONYMOUS_CONNECTION_BY_IP and address not in ["localhost", "127.0.0.1"]:
         same_ips = await ctx[Connection].query(
             "address", address, limit=1000, lock_index=False, lock_rows=False
         )
