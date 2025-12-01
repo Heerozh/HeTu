e1c40ce0450f064af645a9548d21449b2f1c6350
heerozh <heeroz@gmail.com>
2025-12-01 20:25:32 +0800

    MAINT: rename mod_item_component to model

--- tests/test_backend_session_composer.py
+++ tests/test_backend_session_composer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+
 @pytest.mark.xfail(reason="todo: 等idmap")
 async def test_double_upsert(item_table):
     backend = item_table.backend
@@ -9,15 +10,16 @@ async def test_double_upsert(item_table):
     async with backend.transaction(1) as session:
         tbl = item_table.attach(session)
 
-        async with tbl.update_or_insert('itm1', 'name') as row:
+        async with tbl.update_or_insert("itm1", "name") as row:
             row.time = 32345
-        async with tbl.update_or_insert('itm1', 'name') as row:
+        async with tbl.update_or_insert("itm1", "name") as row:
             row.time = 32346
 
     async with backend.transaction(1) as session:
         tbl = item_table.attach(session)
-        async with tbl.update_or_insert('itm1', 'name') as row:
+        async with tbl.update_or_insert("itm1", "name") as row:
             assert row.time == 32346
+
 
 async def test_query_after_update(filled_item_table):
     backend = filled_item_table.backend
@@ -25,27 +27,27 @@ async def test_query_after_update(filled_item_table):
     # update
     async with backend.transaction(1) as session:
         tbl = filled_item_table.attach(session)
-        row = (await tbl.query('owner', 10))[0]
+        row = (await tbl.query("owner", 10))[0]
         old_name = row.name
-        assert (await tbl.select(old_name, where='name')).name == old_name
+        assert (await tbl.select(old_name, where="name")).name == old_name
         row.owner = 11
-        row.name = 'updated'
+        row.name = "updated"
         await tbl.update(row.id, row)
         # 测试能否命中cache
         row = await tbl.select(row.id)
-        assert row.name == 'updated'
+        assert row.name == "updated"
 
     async with backend.transaction(1) as session:
         tbl = filled_item_table.attach(session)
         row = await tbl.select(row.id)  # 测试用numpy type进行select是否报错
-        assert row.name == 'updated'
-        assert (await tbl.query('owner', row.owner, limit=30)).shape[0] == 1
-        assert (await tbl.query('owner', 10, limit=30)).shape[0] == 24
-        assert (await tbl.query('owner', 11)).shape[0] == 1
-        assert (await tbl.query('owner', 11)).name == 'updated'
-        assert (await tbl.select('updated', where='name')).name == 'updated'
-        assert await tbl.select(old_name, where='name') is None
-        assert len(await tbl.query('id', -np.inf, +np.inf, limit=999)) == 25
+        assert row.name == "updated"
+        assert (await tbl.query("owner", row.owner, limit=30)).shape[0] == 1
+        assert (await tbl.query("owner", 10, limit=30)).shape[0] == 24
+        assert (await tbl.query("owner", 11)).shape[0] == 1
+        assert (await tbl.query("owner", 11)).name == "updated"
+        assert (await tbl.select("updated", where="name")).name == "updated"
+        assert await tbl.select(old_name, where="name") is None
+        assert len(await tbl.query("id", -np.inf, +np.inf, limit=999)) == 25
 
 
 async def test_query_after_delete(filled_item_table):
@@ -62,13 +64,13 @@ async def test_query_after_delete(filled_item_table):
 
     async with backend.transaction(1) as session:
         tbl = filled_item_table.attach(session)
-        assert len(await tbl.query('id', -np.inf, +np.inf, limit=999)) == 23
-        assert await tbl.select('Itm14', where='name') is None
-        assert await tbl.select('Itm16', where='name') is None
-        assert (await tbl.query('time', 114, 116)).shape[0] == 1
+        assert len(await tbl.query("id", -np.inf, +np.inf, limit=999)) == 23
+        assert await tbl.select("Itm14", where="name") is None
+        assert await tbl.select("Itm16", where="name") is None
+        assert (await tbl.query("time", 114, 116)).shape[0] == 1
 
 
-async def test_dup_update(mod_item_component, filled_item_table):
+async def test_dup_update(mod_item_model, filled_item_table):
     backend = filled_item_table.backend
     # todo 改成完整的UnitOfWork模式, 使用 Identity Map。不再使用cache
     #      所有数据操作首先经过Identity Map，并每行标记是否为脏（update, insert, delete）状态
@@ -88,7 +90,7 @@ async def test_dup_update(mod_item_component, filled_item_table):
         row = await tbl.select(2)
         row.time = 32345
         await tbl.update(2, row)
-        with pytest.raises(KeyError, match='重复更新'):
+        with pytest.raises(KeyError, match="重复更新"):
             await tbl.update(2, row)
         await session.end_transaction(discard=True)
 
@@ -98,19 +100,19 @@ async def test_dup_update(mod_item_component, filled_item_table):
         row.time = 32345
         await tbl.update(2, row)
         row.time = 42345
-        with pytest.raises(KeyError, match='重复更新'):
+        with pytest.raises(KeyError, match="重复更新"):
             await tbl.update(2, row)
         await session.end_transaction(discard=True)
 
 
-async def test_dup_delect(mod_item_component, filled_item_table):
+async def test_dup_delect(mod_item_model, filled_item_table):
     backend = filled_item_table.backend
 
     # 检测重复删除报错  todo 改成可以顺利执行
     async with backend.transaction(1) as session:
         tbl = filled_item_table.attach(session)
         await tbl.delete(1)
-        with pytest.raises(KeyError, match='重复删除'):
+        with pytest.raises(KeyError, match="重复删除"):
             await tbl.delete(1)
         await session.end_transaction(discard=True)
 
@@ -119,7 +121,7 @@ async def test_dup_delect(mod_item_component, filled_item_table):
         row = await tbl.select(2)
         row.time = 32345
         await tbl.delete(2)
-        with pytest.raises(KeyError, match='再次更新'):
+        with pytest.raises(KeyError, match="再次更新"):
             await tbl.update(2, row)
         await session.end_transaction(discard=True)
 
