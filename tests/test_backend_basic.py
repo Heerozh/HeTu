9d534ab5f60762f89f01fccaecf0f72898699a93
heerozh <heeroz@gmail.com>
2025-12-28 02:43:40 +0800

    TST: 需要mq完成后才能测了

--- tests/test_backend_basic.py
+++ tests/test_backend_basic.py
@@ -11,10 +11,10 @@ from hetu.common.snowflake_id import SnowflakeID
 
 SnowflakeID().init(1, 0)
 
-# # 当前文件不能有其他地方用mod_auto_backend，否则会冲突
-# @pytest.fixture
-# def mod_auto_backend():
-#     pytest.skip("mod_auto_backend 已在本文件禁用")
+# 当前文件不能有其他地方用mod_auto_backend，否则会冲突
+@pytest.fixture
+def mod_auto_backend():
+    pytest.skip("mod_auto_backend 已在本文件禁用")
 
 
 async def test_volatile_table_flush(auto_backend):
@@ -57,58 +57,61 @@ async def test_volatile_table_flush(auto_backend):
 async def test_reconnect(auto_backend, mod_item_model):
     # 因为要用不同的连接flush，所以只能用function scope的auto_backend
     # 且当前文件不能有其他地方用mod_auto_backend，否则会冲突
-    backend_component_table, get_or_create_backend = auto_backend
+    from hetu.data.backend import TableReference, RaceCondition
 
-    backend = get_or_create_backend("save_test")
-    loc_item_table = backend_component_table(
-        mod_item_model, "ItemSaveTestTable", 1, backend
-    )
-    loc_item_table.flush(force=True)
-    loc_item_table.create_or_migrate()
+    backend = auto_backend("flush_test")
+
+    temp_table = TableReference(mod_item_model, "test", 1)
+    table_maint = backend.get_table_maintenance()
+    try:
+        table_maint.create_table(temp_table)
+    except RaceCondition:
+        table_maint.flush(temp_table, force=True)
 
     # 初始化测试数据
-    async with backend.transaction(1) as session:
-        tbl = loc_item_table.attach(session)
+    row = None
+    async with backend.session("test", 1) as session:
+        select = session.select(mod_item_model)
         for i in range(25):
             row = mod_item_model.new_row()
             row.time = i  # 防止unique冲突
             row.name = f"Item_{i}"  # 防止unique冲突
-            await tbl.insert(row)
+            await select.insert(row)
     # 等待replica同步
     await backend.wait_for_synced()
 
     # 测试保存(断开连接）后再读回来
-    async with backend.transaction(1) as session:
-        tbl = loc_item_table.attach(session)
-        await tbl.delete(1)
-        await tbl.delete(9)
-    async with backend.transaction(1) as session:
-        tbl = loc_item_table.attach(session)
-        size = len(await tbl.query("id", -np.inf, +np.inf, limit=999))
+    async with backend.session("test", 1) as session:
+        select = session.select(mod_item_model)
+        row = await select.get(row.id)
+        await select.delete(row.id)
+    async with backend.session("test", 1) as session:
+        select = session.select(mod_item_model)
+        size = len(await select.range("id", -np.inf, +np.inf, limit=999))
 
     # 测试连接关闭
     await backend.close()  # 不close不能重建backend_component_table
     await backend.close()  # 再次close不该报错
     with pytest.raises(ConnectionError):
-        backend.transaction(1)
+        backend.session("test", 1)
     with pytest.raises(ConnectionError):
         backend.post_configure()
     with pytest.raises(ConnectionError):
         await backend.wait_for_synced()
     with pytest.raises(ConnectionError):
-        await backend.requires_head_lock()
-    with pytest.raises(ConnectionError):
         await backend.get_mq_client()
 
     # 重新初始化table和连接后再试
     backend = None
-    loc_item_table = None
-    backend2 = get_or_create_backend("load_test")
+    table_maint = None
+    backend2 = auto_backend("load_test")
 
-    loc_item_table2 = backend_component_table(
-        mod_item_model, "ItemSaveTestTable", 1, backend2
-    )
-    loc_item_table2.create_or_migrate()
-    async with backend2.transaction(1) as session:
-        tbl = loc_item_table2.attach(session)
-        assert len(await tbl.query("id", -np.inf, +np.inf, limit=999)) == size
+    table_maint2 = backend2.get_table_maintenance()
+    try:
+        table_maint2.create_table(temp_table)
+    except RaceCondition:
+        pass
+
+    async with backend2.session("test", 1) as session:
+        select = session.select(mod_item_model)
+        assert len(await select.range("id", -np.inf, +np.inf, limit=999)) == size
