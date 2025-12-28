81f8885b3a23c3d1db7965d93a183a8640ac3813
heerozh <heeroz@gmail.com>
2025-12-28 17:25:12 +0800

    TST: race test新架构初步

--- tests/test_backend_session_race.py
+++ tests/test_backend_session_race.py
@@ -1,29 +1,31 @@
 import pytest
 
+from hetu.common.snowflake_id import SnowflakeID
 
-async def test_race(mod_item_model, item_table):
+
+async def test_race(item_ref, mod_auto_backend):
     import asyncio
     from hetu.data.backend import RaceCondition
 
     # 测试竞态，通过2个协程来测试
-    backend = item_table.backend
+    backend = mod_auto_backend()
 
     # 测试query时，另一个del和update的竞态
-    async with backend.transaction(1) as session:
-        tbl = item_table.attach(session)
-        row = mod_item_model.new_row()
+    async with backend.session("pytest", 1) as session:
+        item_select = session.select(item_ref.comp_cls)
+        row = item_ref.comp_cls.new_row()
         row.owner = 65535
         row.name = "Self"
         row.time = 233874
-        await tbl.insert(row)
-        row.id = 0
+        await item_select.insert(row)
+        row.id = SnowflakeID().next_id()
         row.name = "ForUpdt"
         row.time += 1
-        await tbl.insert(row)
-        row.id = 0
+        await item_select.insert(row)
+        row.id = SnowflakeID().next_id()
         row.name = "ForDel"
         row.time += 1
-        await tbl.insert(row)
+        await item_select.insert(row)
 
     # 重写item_table的query，延迟n秒
     def mock_slow_query(_session):
@@ -37,28 +39,28 @@ async def test_race(mod_item_model, item_table):
         _session._db_query = mock_query
 
     async def query_owner(value):
-        async with backend.transaction(1) as _session:
-            _tbl = item_table.attach(_session)
+        async with backend.session("pytest", 1) as _session:
+            _item_select = _session.select(item_ref.comp_cls)
             mock_slow_query(_tbl)
-            rows = await _tbl.query("owner", value, lock_index=False)
+            rows = await _item_select.range("owner", value)
             print(rows)
 
     async def select_owner(value):
-        async with backend.transaction(1) as _session:
-            _tbl = item_table.attach(_session)
+        async with backend.session("pytest", 1) as _session:
+            _item_select = _session.select(item_ref.comp_cls)
             mock_slow_query(_tbl)
             rows = await _tbl.select(value, "owner")
             print(rows)
 
     async def del_row(name):
-        async with backend.transaction(1) as _session:
-            _tbl = item_table.attach(_session)
+        async with backend.session("pytest", 1) as _session:
+            _item_select = _session.select(item_ref.comp_cls)
             _row = await _tbl.select(name, "name")
             await _tbl.delete(_row.id)
 
     async def update_owner(name):
-        async with backend.transaction(1) as _session:
-            _tbl = item_table.attach(_session)
+        async with backend.session("pytest", 1) as _session:
+            _item_select = _session.select(item_ref.comp_cls)
             _row = await _tbl.select(name, "name")
             _row.owner = 999
             await _tbl.update(_row.id, _row)
@@ -93,8 +95,8 @@ async def test_race(mod_item_model, item_table):
 
     # 测试事务提交时unique的RaceCondition, 在end前sleep即可测试
     async def insert_and_sleep(db, uni_val, sleep):
-        async with backend.transaction(1) as _session:
-            _tbl = item_table.attach(_session)
+        async with backend.session("pytest", 1) as _session:
+            _item_select = _session.select(item_ref.comp_cls)
             _row = mod_item_model.new_row()
             _row.owner = 874233
             _row.name = str(uni_val)
@@ -116,8 +118,8 @@ async def test_race(mod_item_model, item_table):
 
     # 测试事务提交时的watch的RaceCondition
     async def update_and_sleep(db, sleep):
-        async with backend.transaction(1) as _session:
-            _tbl = item_table.attach(_session)
+        async with backend.session("pytest", 1) as _session:
+            _item_select = _session.select(item_ref.comp_cls)
             _row = await _tbl.select("111111", "name")
             _row.time = 874233
             await _tbl.update(_row.id, _row)
@@ -131,8 +133,8 @@ async def test_race(mod_item_model, item_table):
 
     # 测试query后该值是否激发竞态
     async def query_then_update(sleep):
-        async with backend.transaction(1) as _session:
-            _tbl = item_table.attach(_session)
+        async with backend.session("pytest", 1) as _session:
+            _item_select = _session.select(item_ref.comp_cls)
             _rows = await _tbl.query("model", 2)
             await asyncio.sleep(sleep)
             if len(_rows) == 0:
@@ -147,23 +149,23 @@ async def test_race(mod_item_model, item_table):
         await task1
 
 
-async def test_update_or_insert_race_bug(mod_item_model, item_table):
+async def test_update_or_insert_race_bug(item_ref, mod_auto_backend):
     import asyncio
     from hetu.data.backend import RaceCondition
 
-    backend = item_table.backend
+    backend = mod_auto_backend()
 
     # 测试update_or_insert UniqueViolation是否转化为了RaceCondition
     async def main_task():
-        async with backend.transaction(1) as session:
-            tbl = item_table.attach(session)
+        async with backend.session("pytest", 1) as session:
+            item_select = session.select(item_ref.comp_cls)
             async with tbl.update_or_insert("uni_vio", "name") as row:
                 await asyncio.sleep(0.1)
                 row.qty = 1
 
     async def trouble_task():
-        async with backend.transaction(1) as _session:
-            _tbl = item_table.attach(_session)
+        async with backend.session("pytest", 1) as _session:
+            _item_select = _session.select(item_ref.comp_cls)
             row = mod_item_model.new_row()
             row.name = "uni_vio"
             await _tbl.insert(row)
@@ -176,4 +178,3 @@ async def test_update_or_insert_race_bug(mod_item_model, item_table):
 
     # close backend
     await backend.close()
-
