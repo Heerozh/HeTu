d5ac0006f75180aafff41206fab074a12e9ffe72
heerozh <heeroz@gmail.com>
2026-01-04 18:50:46 +0800

    TST: 去掉headlock的相关测试

--- tests/test_cli_start.py
+++ tests/test_cli_start.py
@@ -4,7 +4,6 @@ import sys
 import pytest
 
 from hetu.__main__ import main
-from hetu.data.backend import HeadLockFailed
 
 
 def test_required_parameters():
@@ -25,24 +24,25 @@ def test_required_parameters():
         main()
 
 
-@pytest.mark.timeout(10, method="thread")
-def test_start_with_redis_head_lock(new_clusters_env, mod_redis_backend):
-    # 使用CONFIG_TEMPLATE.yml启动防止模板内容错误
-    cfg_file = os.path.join(os.path.dirname(__file__), "../CONFIG_TEMPLATE.yml")
-    sys.argv[1:] = ["start", "--config", cfg_file]
-    os.chdir(os.path.join(os.path.dirname(__file__)))
-    # 启动6379的redis服务
-    backend_component_table, get_or_create_backend = mod_redis_backend
-    backend = get_or_create_backend(port=6379)
-    backend.requires_head_lock()
-    # 如果HeadLockFailed没触发，会导致服务器启动成功，然后就卡死了，所以要timeout
-    # RuntimeError是sanic在pypi下会报错，也许未来升级了会修复
-    try:
-        with pytest.raises(HeadLockFailed):
-            main()
-    except RuntimeError as e:
-        if "This event loop is already running" in str(e):
-            pytest.xfail(f"Temporary failure: {e}")
-        else:
-            raise
-    backend.close()
+# todo 去掉headlock后的相关测试
+# @pytest.mark.timeout(10, method="thread")
+# def test_start_with_redis_head_lock(new_clusters_env, mod_redis_backend):
+#     # 使用CONFIG_TEMPLATE.yml启动防止模板内容错误
+#     cfg_file = os.path.join(os.path.dirname(__file__), "../CONFIG_TEMPLATE.yml")
+#     sys.argv[1:] = ["start", "--config", cfg_file]
+#     os.chdir(os.path.join(os.path.dirname(__file__)))
+#     # 启动6379的redis服务
+#     backend_component_table, get_or_create_backend = mod_redis_backend
+#     backend = get_or_create_backend(port=6379)
+#     backend.requires_head_lock()
+#     # 如果HeadLockFailed没触发，会导致服务器启动成功，然后就卡死了，所以要timeout
+#     # RuntimeError是sanic在pypi下会报错，也许未来升级了会修复
+#     try:
+#         with pytest.raises(HeadLockFailed):
+#             main()
+#     except RuntimeError as e:
+#         if "This event loop is already running" in str(e):
+#             pytest.xfail(f"Temporary failure: {e}")
+#         else:
+#             raise
+#     backend.close()
